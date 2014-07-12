e7ac49302bcf557f3e8c1f1abac1d234c2f4e302
justinmschw <justinmschw@9b310983-a9b7-e361-9956-2e7711e48517>
2014-07-12 17:55:00 +0000

    Fixed the install script so that it doesn't overwrite existing configuration.

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 # Temporary installation script
-if [ -d /etc/archangel ]; then
-    rm -rf /etc/archangel
+if [ ! -d /etc/archangel ]; then
+    # Don't overwrite existing configuration
+    cp -r etc/archangel /etc/
 fi
 if [ -d /usr/share/archangel ]; then
     rm -rf /usr/share/archangel
@@ -11,7 +12,6 @@ fi
 if [ -e /etc/init.d/archangel ]; then
     rm /etc/init.d/archangel
 fi
-cp -r etc/archangel /etc/
 cp -r usr/share/archangel /usr/share/
 cp -r usr/bin/archangel /usr/bin/
 cp etc/init.d/archangel /etc/init.d/

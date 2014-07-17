abcafc5763c31e362c15a91bf3a6798f9cf7b25b
justinmschw <justinmschw@9b310983-a9b7-e361-9956-2e7711e48517>
2014-07-17 22:57:08 +0000

    Fixed a bug in the youtube blocker. There was a character that was being parsed as a regex operator.

--- usr/share/archangel/matching_modules/banned_youtube.py
+++ usr/share/archangel/matching_modules/banned_youtube.py
@@ -7,9 +7,12 @@ from utils.load_config_file import loadFile
 from utils.blockpage import BlockPage
 from utils.matchresult import MatchResult
 
+MAX_CACHE_SIZE = 2048
+
 # API definitions starts here
 class BannedYoutube:
     def __init__(self, parser):
+        self.clean_cache = {}
         self.ROOT_PREFIX = parser.get('app_config', 'confdir')
         self.category = "Banned Youtube Video"
         self.config_file = self.ROOT_PREFIX + "/lists/bannedyoutubelist"
@@ -22,17 +25,28 @@ class BannedYoutube:
         result = MatchResult()
         url = request.enc_req[1]
         vid_id = ''
-        m = re.search('youtube.com/watch?v=(.){11}', url) 
+        is_youtube=False
+        if 'host' in request.enc_req_headers:
+            for host in request.enc_req_headers['host']:
+                if 'youtube' in host or 'ytimg' in host:
+                    is_youtube=True
+        if not is_youtube:
+            return result
+        m = re.search('watch\?v=(.){11}', url) 
         if m != None:
             vid_id = url[m.start(0):m.end(0)][-11:]
-        m = re.search('youtube.com/v/(.){11}', url)
+        m = re.search('v/(.){11}', url)
         if m != None:
             vid_id = url[m.start(0):m.end(0)][-11:]
-        m = re.search('(ytimg|youtube).com/vi/(.){11}', url)
+        m = re.search('vi/(.){11}', url)
         if m != None:
             vid_id = url[m.start(0):m.end(0)][-11:]
         if vid_id == '':
             # No youtube url present
+            return result
+        # Check cache
+        if vid_id in self.clean_cache:
+            self.clean_cache[vid_id] += 1
             return result
         # Get info on youtube video
         data = urllib2.urlopen('http://gdata.youtube.com/feeds/api/videos/'+vid_id+'?v=2&alt=json').read().lower()
@@ -43,6 +57,12 @@ class BannedYoutube:
                 result.category = self.category
                 result.criteria = regex
                 return result
+        # Clean video; add to cache
+        if len(self.clean_cache) == MAX_CACHE_SIZE:
+            min_cache_key = min(self.clean_cache, key=self.clean_cache.get)
+            del self.clean_cache[min_cache_key]
+        if not vid_id in self.clean_cache:
+            self.clean_cache[vid_id] = 1
         # No match
         return result
     # Dummy method
